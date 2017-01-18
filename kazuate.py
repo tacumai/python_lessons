d109f6d9ac2c248784f6a14892fab56df5f0b7ef
tacumai <tacuma.igei@gmail.com>
2017-01-18 23:34:39 +0900

    add count func

--- kazuate.py
+++ kazuate.py
@@ -1,12 +1,14 @@
 import random
 
 NUM = random.randint(1, 10)
+COUNT = 0
 
 print('数を当ててください。(1〜10の中にあります）')
 
 while True:
   print('入力：', end='')
   n = int(input())
+  COUNT += 1
   if n == NUM:
     break;
   if n > NUM:
@@ -14,3 +16,4 @@ while True:
   else:
     print('もっと大きな数です')
 print('あたりです!おめでとうございます!')
+print('{}回目の入力で一致しました。'.format(COUNT))
